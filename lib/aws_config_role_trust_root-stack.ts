51c7a85e107013a45bfa93e0a1626a4999d88ddb
Raphael <r.schnaitl@gmail.com>
2019-05-23 17:51:12 +0200

    added lambda

--- lib/aws_config_role_trust_root-stack.ts
+++ lib/aws_config_role_trust_root-stack.ts
@@ -1,9 +1,16 @@
 import cdk = require('@aws-cdk/cdk');
+import lambda = require('@aws-cdk/aws-lambda')
 
 export class AwsConfigRoleTrustRootStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    new lambda.Function(this, 'config-rule-root-trust-lambda',{
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NodeJS810,
+      code: lambda.Code.asset('./function')
+    })     
   }
 }
