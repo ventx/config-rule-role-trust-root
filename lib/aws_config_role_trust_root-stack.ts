04803824428980828c7028e175be98c8f4183607
Raphael <r.schnaitl@gmail.com>
2019-05-24 11:28:56 +0200

    added config rule

--- lib/aws_config_role_trust_root-stack.ts
+++ lib/aws_config_role_trust_root-stack.ts
@@ -1,14 +1,27 @@
 import cdk = require('@aws-cdk/cdk');
 import lambda = require('@aws-cdk/aws-lambda')
+import config = require('@aws-cdk/aws-config')
 
 export class AwsConfigRoleTrustRootStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'config-rule-root-trust-lambda',{
+    const ruleFunction = new lambda.Function(this, 'config-rule-root-trust-lambda', {
       handler: 'index.handler',
       runtime: lambda.Runtime.NodeJS810,
       code: lambda.Code.asset('./function')
-    })     
+    })
+
+    new config.CfnConfigRule(this, 'config-role', {
+      source: {
+        owner: 'CUSTOM_LAMBDA',
+        sourceIdentifier: ruleFunction.functionArn,
+        sourceDetails: [
+          {
+            eventSource: 'aws.config',
+            messageType: 'ConfigurationItemChangeNotification'
+          }
+        ]
+      }})
   }
 }
